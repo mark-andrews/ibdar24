a018635255a9ef86446cd1fd98890cdc5f5c89b9
Mark Andrews <mjandrews.org@gmail.com>
2024-06-12 09:14:48 +0100

    Update links

--- data/dl_data.R
+++ data/dl_data.R
@@ -1,12 +1,12 @@
 library(tidyverse)
-biochemists_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/biochemist.csv')
+biochemists_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/biochemist.csv')
 
-smoking_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/smoking.csv')
+smoking_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/smoking.csv')
 
-classroom_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/classroom.csv")
+classroom_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/classroom.csv")
 
-mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieve.csv")
-mathach_school_df <- read_csv <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieveschool.csv")
-science_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/science.csv')
-owls <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/owls.csv')
-weight_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/main/data/weight.csv")
+mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/mathachieve.csv")
+mathach_school_df <- read_csv <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/mathachieveschool.csv")
+science_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/science.csv')
+owls <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar24/master/data/owls.csv')
+weight_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar24/main/data/weight.csv")
